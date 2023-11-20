6bc22be2d63e84a3d883c2edacb096ffa42514da
AgentArachnid66 <50571566+AgentArachnid66@users.noreply.github.com>
2023-11-20 02:53:56 +0000

    Update CommonFunctionLibrary.cpp

--- Private/FunctionLibraries/CommonFunctionLibrary.cpp
+++ Private/FunctionLibraries/CommonFunctionLibrary.cpp
@@ -1,1 +1,27 @@
 
+
+#include "FunctionLibrary/CommonFunctionLibrary.h"
+
+bool UCommonFunctionLibrary::Regenerate(UPARAM(ref) float& Variable, float regenAmount, float maxAmount,  const bool& canRegen) 
+{
+	if (canRegen)
+	{
+		Variable = FMath::Clamp(Variable + regenAmount, 0, maxAmount);
+	}
+
+	if (verbose) {
+	}
+
+	return Variable >= maxAmount;
+
+}
+
+bool UCommonFunctionLibrary::Decay(UPARAM(ref) float& Variable, float decayAmount, float minAmount, const bool& canDecay)
+{
+	if (canDecay)
+	{
+		Variable = FMath::Clamp(Variable - decayAmount, minAmount, Variable);
+	}
+
+	return Variable <= minAmount;
+}
