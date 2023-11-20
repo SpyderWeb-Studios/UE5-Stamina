b8604197ac74b3479d3664e8626e89dbf6c8939e
AgentArachnid66 <50571566+AgentArachnid66@users.noreply.github.com>
2023-11-20 02:52:52 +0000

    Update CommonFunctionLibrary.h

--- Public/FunctionLibraries/CommonFunctionLibrary.h
+++ Public/FunctionLibraries/CommonFunctionLibrary.h
@@ -1,14 +1,17 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
 #include "CommonFunctionLibrary.generated.h"
 
 UCLASS()
 class STAMINA_API UCommonFunctionLibrary : public UBlueprintFunctionLibrary
 {
-  UFUNCTION(BlueprintCallable, Category="Common Function Library")
-  static bool Regenerate(UPARAM(ref) float & Value, float Delta, float MaxValue, const bool& bCanRegen);
+public:
 
-   UFUNCTION(BlueprintCallable, Category="Common Function Library")
-  static bool Decay(UPARAM(ref) float& Value, float Delta, float MinValue, const bool& bCanDecay);
+	UFUNCTION(BlueprintCallable, Category="Common Function Library")
+	static bool Regenerate(UPARAM(ref) float& Variable, float regenAmount, float maxAmount, const bool& canRegen);
+
+	UFUNCTION(BlueprintCallable, Category="Common Function Library")
+	static bool Decay(UPARAM(ref) float& Variable, float decayAmount, float minAmount, const bool& canDecay);
 }
