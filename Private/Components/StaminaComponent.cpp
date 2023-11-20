c7cd8870d0f98e4534118e8a45ad87a1c940ebe9
AgentArachnid66 <50571566+AgentArachnid66@users.noreply.github.com>
2023-11-20 02:45:54 +0000

    Update StaminaComponent.cpp

--- Private/Components/StaminaComponent.cpp
+++ Private/Components/StaminaComponent.cpp
@@ -3,6 +3,7 @@
 
 #include "Components/StaminaComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "FunctionLibraries/CommonFunctionLibrary.h"
 
 // Sets default values for this component's properties
 UStaminaComponent::UStaminaComponent()
@@ -12,15 +13,11 @@ UStaminaComponent::UStaminaComponent()
 
 void UStaminaComponent::OnRep_Stamina()
 {
-	UDebugFunctionLibrary::DebugLogWithObjectContext(this, "Stamina Replicated; " +FString::SanitizeFloat(Stamina), EDebugType::DT_Log, 5.0f);
-
 	OnStaminaValueUpdated.Broadcast(Stamina);
 }
 
 void UStaminaComponent::OnRep_MaxStamina()
 {
-	UDebugFunctionLibrary::DebugLogWithObjectContext(this, "Max Stamina Replicated; " + FString::SanitizeFloat(MaxStamina), EDebugType::DT_Log, 5.0f);
-
 	OnMaxStaminaValueUpdated.Broadcast(MaxStamina);
 }
 
@@ -76,10 +73,9 @@ void UStaminaComponent::StaminaRegenerate()
 {
 	if(GetOwner()->HasAuthority() && !bIsEnabled)
 	{
-		UDebugFunctionLibrary::DebugLogWithObjectContext(this, "Regenerating Stamina");
 		if(RegenerateStamina(StaminaRegenStep))
 		{
-			UDebugFunctionLibrary::DebugLogWithObjectContext(this, "Stamina Fully Regenerated");
+			UE_LOG(LogTemp, Log, TEXT("Stamina Fully Replenished"));
 		}
 	}
 }
@@ -89,7 +85,6 @@ void UStaminaComponent::SetRegenVariables(const float& DecayRate, const float& D
 	// Make sure we are on the server
 	if(!GetOwner()->HasAuthority())
 	{
-		UDebugFunctionLibrary::DebugLogWithObjectContext(this, "SetRegenVariables called on client");
 		return;
 	}
 	
